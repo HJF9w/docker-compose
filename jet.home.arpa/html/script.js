7211b58d6ef1ce0a26096a5083ed176e9a57f9ca
user <user@fedora>
2023-08-29 10:36:32 +0200

    cleaned up javascript, lets see if it works!

--- jet.home.arpa/html/script.js
+++ jet.home.arpa/html/script.js
@@ -3,72 +3,41 @@ const body = document.body;
 
 function setTheme() {
   const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-  if (prefersDark) {
-    body.classList.add('dark-mode');
-    themeToggle.textContent = '🔆';
-  } else {
-    body.classList.remove('dark-mode');
-    themeToggle.textContent = '🌒';
-  }
+  body.classList.toggle('dark-mode', prefersDark);
+  themeToggle.textContent = prefersDark ? '🔆' : '🌒';
 }
 
 themeToggle.addEventListener('click', () => {
   body.classList.toggle('light-mode');
   body.classList.toggle('dark-mode');
-  if (body.classList.contains('dark-mode')) {
-    themeToggle.textContent = '🔆';
-  } else {
-    themeToggle.textContent = '🌒';
-  }
+  themeToggle.textContent = body.classList.contains('dark-mode') ? '🔆' : '🌒';
 });
 
-
 function createBubble() {
-  // Generate random numbers between 0 and 255
   const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+  const colorRanges = [
+    [0, 255, 200, 255, 0, 75],
+    [200, 255, 0, 75, 0, 255],
+    [0, 75, 0, 255, 200, 255]
+  ];
+  
+  const [minRed, maxRed, minGreen, maxGreen, minBlue, maxBlue] = colorRanges[randomNumber(0, 2)];
 
-  // Decide randomly which number is low and which is high
-  const lowIndex = randomNumber(0, 2);
-  const highIndex = randomNumber(0, 2);
+  const section = document.querySelector('section');
+  const createElement = document.createElement('span');
+  const size = Math.random() * 10;
 
-  let randomRed, randomGreen, randomBlue;
-
-  if (lowIndex === 0) {
-    randomRed = randomNumber(0, 255);
-    randomGreen = randomNumber(200, 255);
-    randomBlue = randomNumber(0, 75);
-  } else if (lowIndex === 1) {
-    randomRed = randomNumber(200, 255);
-    randomGreen = randomNumber(0, 75);
-    randomBlue = randomNumber(0, 255);
-  } else {
-    randomRed = randomNumber(0, 75);
-    randomGreen = randomNumber(0, 255);
-    randomBlue = randomNumber(200, 255);
-  }
-
-  //console.log("randomRed:", randomRed);
-  //console.log("randomGreen:", randomGreen);
-  //console.log("randomBlue:", randomBlue);
-  // Acualy start making bubbles, everything until now is just for randome colors.
-  const section = document.querySelector('section')
-  const createElement = document.createElement('span')
-  var size = Math.random() * 10;
-
-  createElement.style.width = 5 + size + 'rem';
-  createElement.style.height = 5 + size + 'rem';
-  createElement.style.left = Math.random() * innerWidth + "px";
-
-  createElement.style.boxShadow = `inset 0 0 2rem rgba(${randomRed}, ${randomGreen}, ${randomBlue}, 0.7)`;
+  createElement.style.width = `${5 + size}rem`;
+  createElement.style.height = `${5 + size}rem`;
+  createElement.style.left = `${Math.random() * innerWidth}px`;
+  createElement.style.boxShadow = `inset 0 0 2rem rgba(${randomNumber(minRed, maxRed)}, ${randomNumber(minGreen, maxGreen)}, ${randomNumber(minBlue, maxBlue)}, 0.7)`;
 
   section.appendChild(createElement);
 
   setTimeout(() => {
     createElement.remove();
-  }, 30000)
+  }, 30000);
 }
 
-
 setTheme();
-setInterval(createBubble, 3000)
+setInterval(createBubble, 3000);
