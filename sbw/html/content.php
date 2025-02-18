3f49d43c1e76e1df2ea933c82a7fcf7b33c6fd03
net@titanite <48803361+HJF9w@users.noreply.github.com>
2025-02-18 17:14:36 +0100

    Cleanup

--- sbw/html/content.php
+++ sbw/html/content.php
@@ -1,18 +1,12 @@
 <?php
-$servername = "172.19.0.3";
+$servername = "db";
 $username = "user";
 $password = "pw4user";
-$dbname = "sbw"; // replace with your actual database name
-
-// Create connection
+$dbname = "sbw";
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-// SQL to create table if it doesn't exist
 $sql = "CREATE TABLE IF NOT EXISTS termine (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     titel VARCHAR(255) NOT NULL,
@@ -21,27 +15,16 @@ $sql = "CREATE TABLE IF NOT EXISTS termine (
     beschreibung TEXT NOT NULL,
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) CHARACTER SET utf8 COLLATE utf8_general_ci";
-
-if ($conn->query($sql) === TRUE) {
-    // Table created successfully or already exists
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
-// Fetch records
+if ($conn->query($sql) === TRUE) {} else {echo "Error creating table: " . $conn->error;}
 $sql = "SELECT titel, datum, ort, beschreibung FROM termine";
 $result = $conn->query($sql);
-
 if ($result->num_rows > 0) {
-    // Output data of each row
     while($row = $result->fetch_assoc()) {
         $titel = htmlspecialchars(trim($row["titel"]));
         $datum = htmlspecialchars(trim($row["datum"]));
         $ort = htmlspecialchars(trim($row["ort"]));
         $beschreibung = htmlspecialchars(trim($row["beschreibung"]));
-        
         $google_maps_link = "https://www.google.com/maps/search/" . str_replace(" ", "+", $ort);
-
         echo '<div class="news">
                 <a href="">
                     <h3>' . $titel . '</h3>
@@ -55,7 +38,6 @@ if ($result->num_rows > 0) {
             </div>';
     }
 } else {
-    // No records found or error reading the table
     echo '<div class="news">
             <div class="left">
             <h4>Aktuell Sind keine Termine Verfügbar, bei fragen Kontaktieren sie bitte Vorname Nachname.</h4>
